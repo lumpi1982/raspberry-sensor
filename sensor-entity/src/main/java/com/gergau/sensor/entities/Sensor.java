82956e9407c299cc9a64d371d78c40ea6ba284cc
gerhard <gerhard@black>
2014-03-16 09:50:34 +0100

    Initial Commit for Entity Project

---
+++ sensor-entity/src/main/java/com/gergau/sensor/entities/Sensor.java
@@ -0,0 +1,66 @@
+package com.gergau.sensor.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Version;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@NamedQueries({ @NamedQuery(name = Sensor.FIND_ALL, query = "select s from Sensor s") })
+public class Sensor {
+	public final static String FIND_ALL = "sensor.findAll";
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@Version
+	private Long version;
+
+	@NotNull
+	private String name;
+
+	@OneToMany(cascade=CascadeType.PERSIST, fetch= FetchType.EAGER)
+	private List<SensorMeasure> measures;
+
+	@NotNull
+	@ManyToOne(cascade = CascadeType.PERSIST)
+	private Location location;
+
+	public Location getLocation() {
+		return location;
+	}
+
+	public List<SensorMeasure> getMeasures() {
+		if (measures == null) {
+			measures = new ArrayList<>();
+		}
+		return measures;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setLocation(Location location) {
+		this.location = location;
+	}
+
+	public void setMeasures(List<SensorMeasure> measures) {
+		this.measures = measures;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
